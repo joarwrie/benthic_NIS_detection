2df809fdcc7a85f23a3c251f49a0074ac91f4f35
joarwrie <67008125+joarwrie@users.noreply.github.com>
2021-07-07 16:19:18 +0200

    Update Demult_cutadapt.sh

--- Demult_cutadapt.sh
+++ Demult_cutadapt.sh
@@ -9,3 +9,22 @@ for i in $(ls *R1_001.fastq.gz);do
 	cutadapt -g file:Demult_barcodes/barcode_COI_forward -G file:Demult_barcodes/barcode_COI_reverse -e 0.12 -o ${Nom}_{name1}_{name2}_lib2_R1_cut.fastq -p ${Nom}_{name1}_{name2}_lib2_R2_cut.fastq $i $reverse
 done
 
+# Select only files with proper tag combinations for further processing
+mkdir inputDADA2
+for i in $(ls *cut.fastq);do
+	if [ $(echo $i | grep -c "_F1_R1_") -eq 1 ];then
+		newname=$(echo $i | sed 's/_F1_R1_/_PCR1_/g')
+		mv $i inputDADA2/$newname
+	elif [ $(echo $i | grep -c "_F2_R2_") -eq 1 ];then
+		newname=$(echo $i | sed 's/_F2_R2_/_PCR2_/g')
+		mv $i inputDADA2/$newname
+	elif [ $(echo $i | grep -c "_F3_R3_") -eq 1 ];then
+		newname=$(echo $i | sed 's/_F3_R3_/_PCR3_/g')
+		mv $i inputDADA2/$newname
+	elif [ $(echo $i | grep -c "_F4_R4_") -eq 1 ];then
+		newname=$(echo $i | sed 's/_F4_R4_/_PCR4_/g')
+		mv $i inputDADA2/$newname
+	elif [ $(echo $i | grep -c "_F5_R5_") -eq 1 ];then
+		newname=$(echo $i | sed 's/_F5_R5_/_PCR5_/g')
+		mv $i inputDADA2/$newname
+	fi
